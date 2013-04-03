bfd3a83885d0528001b60ca7c0c200589d14b550
Jeffrey Tong <trigun0x2@gmail.com>
2013-04-03 19:48:14 -0400

    initial commit but not clean

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SBerryCake::Application.config.secret_token = '5c474b5cdb9f3329e0a853bff032f4b92a134337ddc90cfa248c84d8e287cb5dd6b2947671c06d0382143ecc3bf0f0b5e231cc560f6745075a54aeefe3e5ca4a'
---
+++ spec/models/buss_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Buss do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
